98377968c73e1c9e210c7b2bf21aab58c859aef3
404random <hershgupta98@gmail.com>
2017-02-04 23:31:17 -0500

    final-commit

--- Project-50/src/main/java/Brown_Hack/Project_50/App.java
+++ Project-50/src/main/java/Brown_Hack/Project_50/App.java
@@ -13,7 +13,7 @@ public class App
         ToText test = new ToText();
         ToAudio test1 = new ToAudio();
         try {
-            test1.toRun(test.run("Automatic summarization is the process of reducing a text document with a computer program in order to create a summary that retains the most important points of the original document. As the problem of information overload has grown, and as the quantity of data has increased, so has interest in automatic summarization. Technologies that can make a coherent summary take into account variables such as length, writing style and syntax. An example of the use of summarization technology is search engines such as Google. Document summarization is another.", 2));
+            test1.toRun(test.run("Automatic summarization is the process of reducing a text document with a computer program in order to create a summary that retains the most important points of the original document. As the problem of information overload has grown, and as the quantity of data has increased, so has interest in automatic summarization. Technologies that can make a coherent summary take into account variables such as length, writing style and syntax. An example of the use of summarization technology is search engines such as Google. Document summarization is another.", 2),false,"test_one");
         } catch (UnirestException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
--- Project-50/src/main/java/Brown_Hack/Project_50/ToAudio.java
+++ Project-50/src/main/java/Brown_Hack/Project_50/ToAudio.java
@@ -1,8 +1,11 @@
 package Brown_Hack.Project_50;
 
+import javax.sound.sampled.AudioFileFormat.Type;
+
 import com.sun.speech.freetts.FreeTTSSpeakableImpl;
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
+import com.sun.speech.freetts.audio.SingleFileAudioPlayer;
 
 public class ToAudio {
     private static final String VOICENAME_kevin = "kevin";
@@ -10,13 +13,20 @@ public class ToAudio {
         
     }
     
-    public void toRun(String in){
+    public void toRun(String in,boolean save, String saveLocation ){
         Voice voice;
-        
         VoiceManager voiceManager = VoiceManager.getInstance();
         
         voice = voiceManager.getVoice(VOICENAME_kevin);
         voice.allocate();
+        
+        SingleFileAudioPlayer audioPlayer = new SingleFileAudioPlayer("/Users/hershgupta/Documents/audio/"+saveLocation,Type.WAVE);
+        if(save){
+            
+            voice.setAudioPlayer(audioPlayer);
+        }
         voice.speak(in);
+        voice.deallocate();
+        audioPlayer.close();
     }
 }
